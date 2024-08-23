01bde3d6704b5590f2d308f7bd8ecdd5b803c6ea
　 <>
2024-08-23 09:55:42 +0800

    feat: optimize log

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import type {IndexHtmlTransformHook, PluginOption} from 'vite';
 import * as vite from 'vite';
 import type {Config} from "./type";
 import javascriptObfuscator from 'javascript-obfuscator';
-import {defaultConfig} from "./utils";
+import {defaultConfig, Log} from "./utils";
 
 function getViteMajorVersion() {
   return vite?.version ? Number(vite.version.split('.')[0]) : 2
@@ -10,21 +10,22 @@ function getViteMajorVersion() {
 
 export default function viteBundleObfuscator(config?: Partial<Config>): PluginOption {
   const finalConfig = {...defaultConfig, ...config};
+  const _log = new Log(finalConfig.log);
 
   const transformIndexHtmlHandler: IndexHtmlTransformHook = (html, {bundle}) => {
     if (!finalConfig.enable || !bundle) return html;
 
     const now = performance.now();
-    console.log('starting obfuscation process...');
+    _log.alwaysLog('starting obfuscation process...');
     Object.entries(bundle).forEach(([fileName, bundleItem]) => {
       if ('code' in bundleItem && bundleItem.code && finalConfig.excludes.every(exclude => !fileName.includes(exclude))) {
-        if (finalConfig.log) console.log(`Obfuscating ${fileName}...`);
+        _log.info(`obfuscating ${fileName}...`);
         bundleItem.code = javascriptObfuscator.obfuscate(bundleItem.code, finalConfig.options).getObfuscatedCode();
-        if (finalConfig.log) console.log(`Obfuscation complete for ${fileName}`);
+        _log.info(`obfuscation complete for ${fileName}.`);
       }
     });
     const consume = (performance.now() - now) / 1000;
-    console.log('obfuscation process completed in ' + consume.toFixed(1) + 's');
+    _log.alwaysLog('✓ obfuscation process completed in ' + consume.toFixed(1) + 's.');
 
     return html;
   }
