01d1a2cd81b5b31b0454e06f6412eb72cf93669b
z0ffy <654109707@qq.com>
2024-10-15 23:40:27 +0800

    feat: add vitest

--- __tests__/plugin.spec.ts
+++ __tests__/plugin.spec.ts
@@ -1,6 +1,6 @@
 import {expect, describe, it, vi} from "vitest";
-import {formatTime, getThreadPoolSize, Log} from "../src/utils";
-import {Config} from "../src/type";
+import {formatTime, getThreadPoolSize, getValidBundleList, Log} from "../src/utils";
+import {BundleList, Config} from "../src/type";
 
 const defaultConfig: Config = {
   enable: true,
@@ -91,5 +91,60 @@ describe('getThreadPoolSize', () => {
   it('should return default CPU count when threadPool size is 0', () => {
     const configWithSizeZero = {...defaultConfig, threadPool: {enable: true, size: 0}};
     expect(getThreadPoolSize(configWithSizeZero)).toBeTypeOf('number');
+  });
+});
+
+describe('getValidBundleList', () => {
+  it('should return valid bundle items', () => {
+    const finalConfig: Config = {
+      ...defaultConfig,
+      excludes: ['excluded-file.js'],
+    };
+
+    const bundle = {
+      'valid-file.js': {code: 'console.log("valid")'},
+      'excluded-file.js': {code: 'console.log("excluded")'},
+      'invalid-file.js': {notCode: 'no code here'}
+    };
+
+    const expected: BundleList = [
+      // @ts-ignore
+      ['valid-file.js', {code: 'console.log("valid")'}]
+    ];
+
+    // @ts-ignore
+    const result = getValidBundleList(finalConfig, bundle);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an empty array if no valid bundle items exist', () => {
+    const finalConfig: Config = {...defaultConfig, excludes: []};
+
+    const bundle = {
+      'invalid-file.js': {notCode: 'no code here'},
+      'another-invalid-file.js': {notCode: 'still no code'}
+    };
+
+    // @ts-ignore
+    const result = getValidBundleList(finalConfig, bundle);
+    expect(result).toEqual([]);
+  });
+
+  it('should exclude files based on the exclude configuration', () => {
+    const finalConfig: Config = {...defaultConfig, excludes: ['excluded-file.js']};
+
+    const bundle = {
+      'excluded-file.js': {code: 'console.log("excluded")'},
+      'included-file.js': {code: 'console.log("included")'}
+    };
+
+    const expected: BundleList = [
+      // @ts-ignore
+      ['included-file.js', {code: 'console.log("included")'}]
+    ];
+
+    // @ts-ignore
+    const result = getValidBundleList(finalConfig, bundle);
+    expect(result).toEqual(expected);
   });
 });
